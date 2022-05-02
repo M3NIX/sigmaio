439045b8e20fdcd7c757c63798b1082ae8f8c870
M3NIX <julian@ortel.tech>
2022-05-02 23:51:20 +0200

    render selection dynamic

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from flask import Flask, render_template
+
+from sigma.cli.backends import backends
+from sigma.cli.pipelines import pipelines
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+   return render_template('index.html', backends=backends, pipelines=pipelines)
 if __name__ == '__main__':
    app.run()
